b4d325e98b655756ed7bff9e778715f108d808ed
ajay9679 <ajay.katlyn@gmail.com>
2021-07-19 14:54:14 +0530

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [tasks, setTasks] = React.useState([]);
+
+    const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+    React.useEffect(() => {
+        const transformTasks = tasksObj => {
+            const loadedTasks = [];
+            for (const taskKey in tasksObj)
+                loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+            setTasks(loadedTasks);
+        };
+        fetchTasks(
+            { url: 'https://section-15-35028-default-rtdb.firebaseio.com/tasks.json' },
+            transformTasks
+        );
+    }, [fetchTasks]);
+    const taskAddHandler = task => setTasks(prevTasks => prevTasks.concat(task));
+
+    return <React.Fragment>
+        <NewTask onAddTask={taskAddHandler} />
+        <Tasks items={tasks} loading={isLoading} error={error} onFetch={fetchTasks}/>
+    </React.Fragment>
+};
 
 export default App;
---
+++ src/components/Tasks/TaskItem.js
@@ -0,0 +1,5 @@
+import classes from './TaskItem.module.css';
+
+const TaskItem = props => <li className={classes.task}>{props.children}</li>
+
+export default TaskItem;
